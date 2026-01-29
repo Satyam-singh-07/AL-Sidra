7cc3e8de4a344f8828b616f4abed5ae691d61111
satyam <satyamsingh962572@gmail.com>
2026-01-29 16:38:08 +0530

    api

--- routes/api.php
+++ routes/api.php
@@ -30,48 +30,48 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::prefix('user')->group(function(){
+Route::prefix('user')->group(function () {
 
-    Route::post('signup/send-otp',[AuthController::class,'userSignup']);
-    Route::post('signup/verify-otp',[AuthController::class,'verifyUserOtp']);
-    
-    Route::middleware('auth:sanctum')->group( function(){
-        Route::get('profile',[AuthController::class,'userProfile']);
+    Route::post('signup/send-otp', [AuthController::class, 'userSignup']);
+    Route::post('signup/verify-otp', [AuthController::class, 'verifyUserOtp']);
+
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::get('profile', [AuthController::class, 'userProfile']);
+        Route::post('masjid', [MasjidController::class, 'store']);
     });
 });
 
-Route::prefix('member')->group(function(){
+Route::prefix('member')->group(function () {
 
-    Route::post('signup/send-otp',[AuthController::class,'sendMemberOtp']);
-    Route::post('signup/verify-otp',[AuthController::class,'verifyOtpAndCreateMember']);
+    Route::post('signup/send-otp', [AuthController::class, 'sendMemberOtp']);
+    Route::post('signup/verify-otp', [AuthController::class, 'verifyOtpAndCreateMember']);
 
-    Route::middleware('auth:sanctum')->group(function(){
-        Route::get('profile',[AuthController::class,'memberProfile']);
-        Route::post('kyc',[MemberController::class,'store']);
-        Route::post('kyc/submit',[MemberController::class,'submit']);
-        Route::post('masjid',[MasjidController::class,'store']);
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::get('profile', [AuthController::class, 'memberProfile']);
+        Route::post('kyc', [MemberController::class, 'store']);
+        Route::post('kyc/submit', [MemberController::class, 'submit']);
+        Route::post('masjid', [MasjidController::class, 'store']);
     });
 });
 
-Route::get('member-categories',[MemberCategoryController::class,'show']);
-Route::get('masjid',[MasjidController::class,'show']);
-Route::get('madarsa',[MadarsaController::class,'show']);
-Route::post('login/send-otp',[AuthController::class,'userSendLoginOtp']);
-Route::post('login/verify-otp',[AuthController::class,'userVerifyOtp']);
-Route::get('banners',[BannerController::class,'getBanners']);
+Route::get('member-categories', [MemberCategoryController::class, 'show']);
+Route::get('masjid', [MasjidController::class, 'show']);
+Route::get('madarsa', [MadarsaController::class, 'show']);
+Route::post('login/send-otp', [AuthController::class, 'userSendLoginOtp']);
+Route::post('login/verify-otp', [AuthController::class, 'userVerifyOtp']);
+Route::get('banners', [BannerController::class, 'getBanners']);
 Route::get('hot-topics', [HotTopicController::class, 'index']);
 Route::get('hot-topics/{hotTopic}/updates', [TopicUpdateController::class, 'index']);
-Route::get('quran/by-surah',[StaticDataController::class,'surah']);
-Route::get('quran/by-page',[StaticDataController::class,'pages']);
-Route::get('quran/by-juzs',[StaticDataController::class,'juzs']);
-Route::get('quran/by-hizb',[StaticDataController::class,'hizb']);
-Route::get('stac',[StaticDataController::class,'stac']);
+Route::get('quran/by-surah', [StaticDataController::class, 'surah']);
+Route::get('quran/by-page', [StaticDataController::class, 'pages']);
+Route::get('quran/by-juzs', [StaticDataController::class, 'juzs']);
+Route::get('quran/by-hizb', [StaticDataController::class, 'hizb']);
+Route::get('stac', [StaticDataController::class, 'stac']);
 Route::get('ongoing-works', [OngoingWorkApiController::class, 'index']);
 Route::get('ongoing-works/{id}', [OngoingWorkApiController::class, 'show']);
-Route::get('religious-info',[ReligiousInfoController::class,'index']);
-Route::get('videos',[VideoController::class,'index']);
-Route::middleware('auth:sanctum')->group(function(){
+Route::get('religious-info', [ReligiousInfoController::class, 'index']);
+Route::get('videos', [VideoController::class, 'index']);
+Route::middleware('auth:sanctum')->group(function () {
 
-    Route::post('logout',[AuthController::class,'logout']);
-
+    Route::post('logout', [AuthController::class, 'logout']);
 });
